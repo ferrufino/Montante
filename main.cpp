1eb6ab515c558e0fdad7c6d9b0f45fcb6eb433e2
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2013-11-07 17:46:26 -0600

    Commented multiple ecua. validation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  Montante
-//
-//  Created by Gustavo Ferrufino on 10/29/13.
-//  Copyright (c) 2013 Gustavo Ferrufino. All rights reserved.
-//
-
 #include <iostream>
 using namespace std;
 
@@ -17,6 +9,7 @@ int arr[100];
 int pivAct;
 int pivAnt=1;
 bool despl = true;
+bool ckeck= true;
 void desplegar(){
     
     for (int i = 0; i < n; i++) {
@@ -46,72 +39,72 @@ void cambiarRenglon(int temp, int a){
 
 
 bool checaArriba(int p){
-	if (p != 0) {
-		return true;
-	}
-	return false;
+    if (p != 0) {
+        return true;
+    }
+    return false;
 }
 bool checaAbajo(int p) {
-	if (p <= n - 1) {
-		return true;
-	}
-	return false;
+    if (p <= n - 1) {
+        return true;
+    }
+    return false;
 }
 void montanteCuarto(int p){
-	int b = 1;
-	int a = 1;
-	int i = p + 1;
-	int j = p + 1;
+    int b = 1;
+    int a = 1;
+    int i = p + 1;
+    int j = p + 1;
     //cout<<"DEBUG:: Entra opercion Montante Cuarto"<<endl;
-	while(i < n) {
-		while(j < n+1) {
-			matriz[p + b][p + a] = (matriz[p][p] * matriz[p + b][p + a] - matriz[p + b][p] * matriz[p][p + a]) / pivAnt;
-			a++;
-			j++;
-		}
-		j = p + 1;
-		b++;
-		a = 1;
-		i++;
-	}
-	
-	int h = p + 1;
-	while(h < n){
-		matriz[h][p] = 0;
-		h++;
-	}
-	
+    while(i < n) {
+        while(j < n+1) {
+            matriz[p + b][p + a] = (matriz[p][p] * matriz[p + b][p + a] - matriz[p + b][p] * matriz[p][p + a]) / pivAnt;
+            a++;
+            j++;
+        }
+        j = p + 1;
+        b++;
+        a = 1;
+        i++;
+    }
+    
+    int h = p + 1;
+    while(h < n){
+        matriz[h][p] = 0;
+        h++;
+    }
+    
 }
 void montantePrimer(int p) {
-	int f = 1;
-	int g = 1;
-	int b = p + 1;
-	
+    int f = 1;
+    int g = 1;
+    int b = p + 1;
+    
     //cout<<"DEBUG:: Entra opercion Montante Primer"<<endl;
     for(int i = 0; i < p; i++){
-		matriz[i][i] = pivAct;
-		
-	}
-    
-    
-	for(int i = 0; i < p; i++){
-		while(b < n + 1){
-           
-			matriz[p - f][p + g] = (matriz[p][p] * matriz[p -f][p + g] - matriz[p - f][p] * matriz[p][p + g]) / pivAnt;
-			g++;
-			b++;
-			
-		}
-		b = p + 1;
-		g = 1;
-		f++;
+        matriz[i][i] = pivAct;
         
-	}
+    }
     
-	for(int i = 0; i < p; i++){
-		
-		matriz[i][p] = 0;
-	}
+    
+    for(int i = 0; i < p; i++){
+        while(b < n + 1){
+            
+            matriz[p - f][p + g] = (matriz[p][p] * matriz[p -f][p + g] - matriz[p - f][p] * matriz[p][p + g]) / pivAnt;
+            g++;
+            b++;
+            
+        }
+        b = p + 1;
+        g = 1;
+        f++;
+        
+    }
+    
+    for(int i = 0; i < p; i++){
+        
+        matriz[i][p] = 0;
+    }
     
 }
 void respuesta(){
@@ -128,16 +121,16 @@ void respuesta(){
 }
 
 void montante(){
-   
+    
     //cout<<"DEBUG:: Entra opercion Montante"<<endl;
-int x;
-bool check=false;
+    int x;
+    bool check=false;
     
     
     for (int p = 0; p < n; p++)
     {
         pivAct = matriz[p][p];
-      
+        
         x=p+1;
         if (pivAct == 0) {
             while (x<n && check == false) {
@@ -150,57 +143,81 @@ bool check=false;
                 x++;
             }
             
-          
+            
         }
         
         if (matriz[p][p] == 0) {
             cout<<"NO UNICA"<<endl;
             pivAnt=1;
             despl=false;
+            check = false;
             break;
         }
         else{
-        
-        if(checaAbajo(p)){
-            montanteCuarto(p);
-        }
-        
-        if(checaArriba(p)){
-            montantePrimer(p);
-        }
-        pivAnt = pivAct;
+            
+            if(checaAbajo(p)){
+                montanteCuarto(p);
+            }
+            
+            if(checaArriba(p)){
+                montantePrimer(p);
+            }
+            pivAnt = pivAct;
         }
         
     }
     
-    if (matriz[n-1][n-1] == 0) {
+    if (matriz[n-1][n-1] == 0 && check)  {
         cout<<"NO UNICA"<<endl;
         despl=false;
-    
+        
     }
     else if(despl) {
-    
+        
         respuesta();
-    }    
+    }
+    check = true;
     
 }
 void validarMultiplo(){
     int temp=0;
-    bool same = true;
+    int a,b;
+    bool same = false;
     while (temp<n) {
-     
-        for (int i =0; i<n+1; i++) {
-            if ((matriz[i][temp]) != matriz[i+1][temp] ) {
-                same=false;
-            }
-        }
+        
+        /*  for (int i =0; i<n+1; i++) {
+         a=matriz[i][temp];
+         b=matriz[i+1][temp];
+         // cout<<"DEBUG:: a: "<<a<<" y b: "<<b<<endl;
+         /*  if ( (b == 0 && a == 1) || ( b == 1 && a== 0) ) {
+         same = false;
+         break;
+         }
+         else if ((matriz[i][temp]) > matriz[i+1][temp] ) {
+         if(!(a % b) || !(a != b)){
+         same=false;
+         break;
+         
+         }
+         else{
+         if(!(b % a) || !(a != b)){
+         same=false;
+         break;
+         
+         }
+         }
+         
+         }
+         
+         
+         } */
         temp++;
     }
     if (same) {
         //desplegar();
-        cout<<"NO UNICA"<<endl;
+        cout<<"NO UNICA 3"<<endl;
         pivAnt=1;
-
+        
         despl=false;
     }
     else{
@@ -209,8 +226,8 @@ void validarMultiplo(){
         
         montante();
     }
-
-
+    
+    
 }
 
 
@@ -232,13 +249,13 @@ void vaciar(){//vacia y despliega datos
     
 }
 void validacionMontante(){
-
+    
     pivAct = matriz[0][0];
     int pos = 0;
     if (pivAct == 0) {
         int temp = 0;
         bool cambio= false;
-       // cout<<"DEBUG::PRIMER Pivote = 0"<<endl;
+        // cout<<"DEBUG::PRIMER Pivote = 0"<<endl;
         while (temp<n || cambio == false) {
             if (matriz[temp][pos] != 0) {
                 cambio = true;
@@ -251,7 +268,7 @@ void validacionMontante(){
             //desplegar();
             cout<<"NO UNICA"<<endl;
             pivAnt=1;
-
+            
             despl=false;
         }
         else{
@@ -265,35 +282,35 @@ void validacionMontante(){
         //cout<<"DEBUG:: valida- son multiplos? v.2"<<endl;
         validarMultiplo();
     }
-
+    
 }
 int main()
 {
     //n=numero de ecuaciones
     cin >> n;
-	while (n != 0)
-	{
+    while (n != 0)
+    {
         
-		
-		vaciar();
         
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n+1; j++)
-			{
-				cin>>x;
-				matriz[i][j]=x;
+        vaciar();
+        
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n+1; j++)
+            {
+                cin>>x;
+                matriz[i][j]=x;
                 
-			}
-		}
+            }
+        }
         
         validacionMontante();
-       
+        
         cin >> n;
         despl=true;
-       
+        
         
     }
-	
-	return 0;
+    
+    return 0;
 }
